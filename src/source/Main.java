791ac3758e65dc5bd918e0c31185dc7278d90ea2
Kousha Motazedian <kousha@Koushas-MacBook-Pro.local>
2023-01-30 17:25:55 -0500

    Removed print statement

--- src/source/Main.java
+++ src/source/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	
 	public static void main (String[] args)
 	{
-		System.out.print("THIS IS THE MAIN BRANCH");
 		
 		//THIS IS TO TEST FLOOR DELETE WHEN MERGING
 		Scheduler s = new Scheduler();
