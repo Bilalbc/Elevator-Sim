0e6d9fc4953c334d5e6b4cfe1f2061713ed910bd
Matthew Parker <chaosdemon12345@gmail.com>
2023-02-01 22:47:25 -0500

    Added functionality with Scheduler and Comments
    JavaDoc Comments were added along with functionality with scheduler

--- src/source/Floor.java
+++ src/source/Floor.java
@@ -3,15 +3,30 @@ package source;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+/**
+ * Floor object which reads instructions from file and passes it to the elevator scheduler
+ * 
+ * @author Kousha Motazedian, Matthew Parker
+ * @version 1.0
+ * @date Febuary 4th, 2023
+ */
 public class Floor implements Runnable{
 	Scheduler scheduler;
 	
+	/**
+	 * Constructor for Floor class
+	 * @param sch, Scheduler being used
+	 */
 	public Floor(Scheduler sch) {
 		this.scheduler = sch;
 	}
 	
-	
+	/**
+	 * Method to create a request message by reading file
+	 * 
+	 * @param reader, File reader being used
+	 * @return req, Request information, from file
+	 */
 	private Message createRequest(Scanner reader) {
 
 		String data = reader.nextLine();
@@ -22,6 +37,11 @@ public class Floor implements Runnable{
 	}
 	
 	@Override
+	/**
+	 * Run method for Floor class,
+	 * Scans file using reader, and checks for instructions, and if there are, that is passed in a message
+	 * to the scheduler, and then the reply from the elevator is printed
+	 */
 	public void run() {
 		try {
 			File file = new File("src//source//Requests.csv");
@@ -29,7 +49,8 @@ public class Floor implements Runnable{
 			while(reader.hasNextLine()) {
 				Message req = createRequest(reader);
 				System.out.println(req.getDirection() + " " + req.getTime() + " " + req.destinationFloor() + " "+req.startFloor());
-				//send to SCH
+				scheduler.passMessage(req);
+				System.out.println(scheduler.readReply());
 				
 			}
 			reader.close();
