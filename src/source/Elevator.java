470217708657b8e4a1786566e5f150c0e8717ec8
Kousha Motazedian <72173282+Kousha-Motazedian@users.noreply.github.com>
2023-02-16 19:20:47 -0500

    Added elevator functions (#17)

--- src/source/Elevator.java
+++ src/source/Elevator.java
@@ -3,7 +3,7 @@ package source;
 import java.util.ArrayList;
 /**
  * An Elevator class that takes requests from the scheduler and passes gives a reply back for the Floor.
- * @author Akshay V.
+ * @author Akshay V., Kousha Motazedian, Matthew Parker
  * @version 2.0
  * @date February 5th, 2023
  *
@@ -14,7 +14,10 @@ public class Elevator implements Runnable{
     private int currentFloor; 
     private int assignedNum;
     private Scheduler scheduler;
-    private ArrayList<Message> requestQueue;
+    private ArrayList<Integer> destinationQueue;
+    private enum elevatorStates {DOORSOPEN, DOORSCLOSED, MOVINGUP, MOVINGDOWN, STOPPED};
+    private elevatorStates currState;
+    
 
     /**
      * Constructor for Elevator
@@ -23,8 +26,9 @@ public class Elevator implements Runnable{
     public Elevator(Scheduler sch) {
         this.scheduler = sch;
         this.currentFloor = 1;
-        requestQueue = new ArrayList<>();
+        destinationQueue = new ArrayList<>();
         this.assignedNum = 1; //only have 1 elevator right now
+        this.currState = elevatorStates.DOORSCLOSED; //Elevator starts at a closed state
     }
 
     @Override
@@ -34,12 +38,39 @@ public class Elevator implements Runnable{
      */
     public void run() {
     	while (!scheduler.isClosed()) {
-    		Message req = scheduler.readMessage();
-            System.out.println("Recieved message: " + req);
-    		
-            System.out.println("Passing reply: " + req);
-            req.setReturnMessage("Arrived at Destination Floor - Request was: " + req);
-    		scheduler.passReply(req); 
+    		this.currState = elevatorStates.DOORSCLOSED; //Door starts off closed
+    		if(!(destinationQueue.isEmpty())){
+	    		for(int i : destinationQueue) {
+	    			if(i == currentFloor) {
+	    				currState = elevatorStates.STOPPED;
+	    				currState = elevatorStates.DOORSOPEN; //If there is a destination request for the current floor, the doors open
+	    			}
+	    		}
+    		}
+    		destinationQueue.clear();
+    		destinationQueue = scheduler.readMessage().clone();
+            System.out.println("Recieved Destinations");
+            this.currState = elevatorStates.DOORSCLOSED;
+            
+            if(destinationQueue.get(0) > currentFloor) { // check if the elevator needs to move up or down
+            	currState = elevatorStates.MOVINGUP;
+            }
+            else {
+            	currState = elevatorStates.MOVINGDOWN;
+            }
+            
+            System.out.println("Elevator is Moving");
+            //Moves up or down depending on the State of the elevator
+            if(currState.equals(elevatorStates.MOVINGUP)) {
+            	currentFloor++;
+            }
+            if(currState.equals(elevatorStates.MOVINGDOWN)) {
+            	currentFloor--;
+            }
+            
+             //Elevator reached a floor
+            
+            scheduler.passReply(currentFloor, currState);
     		
     		try {
 				Thread.sleep(1000);
